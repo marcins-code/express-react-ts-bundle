0f84fc590df448ecfeabdcd2a47a8957775cd8b5
marcin's-code <mpaczkowski40@gmail.com>
2021-09-06 17:49:56 +0200

    Add router file for Users endpoints

---
+++ app_backend/src/Router/userRouter.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import AuthenticationController from '../Controller/AuthenticationController';
+import UserController from '../Controller/UserController';
+
+const router = Router();
+
+// router.get('/user', UserController.getAllUsers);
+router.get('/user/:id', UserController.getSingleUserById);
+router.get('/admin/user/:id', UserController.getSingleUserById);
+
+router.get('/user', UserController.getPaginatedUsers);
+router.get('/admin/user', UserController.getPaginatedUsers);
+
+router.post('/user/signUp', AuthenticationController.signUp);
+router.post('/user/login', AuthenticationController.login);
+// router.put('/user/update/:id', UserController.update);
+
+export default router;
