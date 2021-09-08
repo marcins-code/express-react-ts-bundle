de2b5f8a743781a1141c78e169150167ae542d0b
marcin's-code <mpaczkowski40@gmail.com>
2021-09-08 17:55:30 +0200

    Add router

--- app_frontend/src/App.tsx
+++ app_frontend/src/App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalTemplate from './templates/GlobalTemplate';
+import Homepage from './views/pages/Homepage';
+import Categories from './views/pages/Categories';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <GlobalTemplate>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/categories" exact component={Categories} />
+      </GlobalTemplate>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
