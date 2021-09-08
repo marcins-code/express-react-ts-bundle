ea4858c146cad93888defd09e435cea96fd5cff7
marcin's-code <mpaczkowski40@gmail.com>
2021-09-08 17:55:02 +0200

    Moved tests

--- app_frontend/src/test/App.test.tsx
+++ app_frontend/src/test/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 // @ts-ignore
-import App from './App.tsx';
+import App from '../App.tsx';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
