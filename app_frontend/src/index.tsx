49da71f8eb4deeac8880686d43ff80a4e5f39264
marcin's-code <mpaczkowski40@gmail.com>
2021-09-17 18:12:30 +0200

    Change icons registering

--- app_frontend/src/index.tsx
+++ app_frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import registerIcons from './Icons/Icons';
+
+registerIcons();
 
 ReactDOM.render(
   <React.StrictMode>
