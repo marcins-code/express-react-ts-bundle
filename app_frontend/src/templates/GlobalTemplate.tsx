3a6cbf923fc754aad253316021951193904ccc40
marcin's-code <mpaczkowski40@gmail.com>
2021-09-17 18:13:59 +0200

    Add all variables to PageContext

--- app_frontend/src/templates/GlobalTemplate.tsx
+++ app_frontend/src/templates/GlobalTemplate.tsx
@@ -4,24 +4,53 @@ import { ThemeMixer } from '../themes/ThemeMixer';
 import { PageContext } from '../context';
 import GlobalStyle from '../themes/GlobalStyle';
 import { useAppSettings } from '../hooks/useAppSettings';
+import SettingsPanel from '../components/organisms/SettingsPanel/SettingsPanel';
 
 type Props = {
     children: React.ReactNode;
 };
+
 const GlobalTemplate: React.FC<Props> = ({ children }) => {
   const pageInitSettings = {
     appTheme: 'dark',
+    appLanguage: 'pl',
+    navPosition: 'menu-top',
+    isAdminPage: false,
   };
 
-  const { appTheme, appThemeHandler } = useAppSettings(pageInitSettings);
+  const {
+    appTheme,
+    appThemeHandler,
+    appLanguage,
+    langSwitcherHandler,
+    navPosition,
+    navPositionHandler,
+    isMobile,
+    rememberSettings,
+    rememberSettingsHandler,
+    isAdminPage,
+  } = useAppSettings(pageInitSettings);
+
   const theme = ThemeMixer(appTheme, pageInitSettings);
-  console.log(theme);
   return (
     <PageContext.Provider
-      value={{ appTheme, appThemeHandler }}
+      value={{
+        appTheme,
+        appThemeHandler,
+        appLanguage,
+        langSwitcherHandler,
+        navPosition,
+        navPositionHandler,
+        isMobile,
+        rememberSettings,
+        rememberSettingsHandler,
+        isAdminPage,
+      }}
     >
+
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <SettingsPanel />
         <>{children}</>
       </ThemeProvider>
     </PageContext.Provider>
