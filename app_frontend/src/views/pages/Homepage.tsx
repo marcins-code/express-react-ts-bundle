b1da826627834f2553e39f09425513cdd8028259
marcin's-code <mpaczkowski40@gmail.com>
2021-09-19 08:51:38 +0200

    Add navigation links

--- app_frontend/src/views/pages/Homepage.tsx
+++ app_frontend/src/views/pages/Homepage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import MainTemplate from '../../templates/MainTemplate';
-import Switcher from '../../components/Atoms/Switcher/Switcher';
+import BlogCard from '../../components/organisms/BlogCard/BlogCard';
 
 const Homepage = () => (
   // @ts-ignore
   <MainTemplate>
-    <h3>Homepage</h3>
-    <Switcher name="mySwitch" switchColor="red" notCheckedColor="indygo" defaultChecked value="nn" onChange={() => {}} />
+    <BlogCard
+      src="https://images.unsplash.com/photo-1517148815978-75f6acaaf32c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2200&q=80"
+      title="Idque Caesaris facere voluntate liceret: sese habere."
+    />
+    <BlogCard
+      src="https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2200&q=80"
+      title="A Complete Guide to Grid"
+    />
   </MainTemplate>
 );
 
