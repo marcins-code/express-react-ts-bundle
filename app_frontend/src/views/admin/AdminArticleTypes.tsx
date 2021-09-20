d46ff50ec221bd50fe17aeb27a3b11872f7156fa
marcin's-code <mpaczkowski40@gmail.com>
2021-09-20 17:58:19 +0200

    Add AdminArticleTypes empty component

---
+++ app_frontend/src/views/admin/AdminArticleTypes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainTemplate from '../../templates/MainTemplate';
+
+const AdminArticleTypes = () => (
+  <MainTemplate>
+    <h3>ArticleTypes</h3>
+  </MainTemplate>
+);
+
+export default AdminArticleTypes;
