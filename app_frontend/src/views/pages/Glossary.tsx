ec175047497f257bb67d3d6b59d9792c9938955b
marcin's-code <mpaczkowski40@gmail.com>
2021-09-20 17:55:06 +0200

    Add Glossary empty component

---
+++ app_frontend/src/views/pages/Glossary.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainTemplate from '../../templates/MainTemplate';
+
+const Glossary = () => (
+  <MainTemplate>
+    <h3>Glossary</h3>
+  </MainTemplate>
+);
+
+export default Glossary;
