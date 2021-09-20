fe4fbc0aeed2cfd8c4381991e09030bd5e9abf66
marcin's-code <mpaczkowski40@gmail.com>
2021-09-20 17:54:47 +0200

    Add Contact empty component

---
+++ app_frontend/src/views/pages/Contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainTemplate from '../../templates/MainTemplate';
+
+const Contact = () => (
+  <MainTemplate>
+    <h3>Contact</h3>
+  </MainTemplate>
+);
+
+export default Contact;
