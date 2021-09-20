cc2af2b774dc4c27f872315e3b556a36a79466bc
marcin's-code <mpaczkowski40@gmail.com>
2021-09-20 17:55:23 +0200

    Add Series empty component

---
+++ app_frontend/src/views/pages/Series.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainTemplate from '../../templates/MainTemplate';
+
+const Series = () => (
+  <MainTemplate>
+    <h3>Series</h3>
+  </MainTemplate>
+);
+
+export default Series;
