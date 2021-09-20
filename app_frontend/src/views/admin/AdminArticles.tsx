02b7c207f92791a2ce0362abbdf79d501d21d69a
marcin's-code <mpaczkowski40@gmail.com>
2021-09-20 17:58:00 +0200

    Add AdminArticles empty component

---
+++ app_frontend/src/views/admin/AdminArticles.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainTemplate from '../../templates/MainTemplate';
+
+const AdminArticles = () => (
+  <MainTemplate>
+    <h3>Articles</h3>
+  </MainTemplate>
+);
+
+export default AdminArticles;
