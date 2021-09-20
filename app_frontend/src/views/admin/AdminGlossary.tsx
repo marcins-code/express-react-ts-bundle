59994b60097f3b8db3bedc0e1d5a1491b263ef26
marcin's-code <mpaczkowski40@gmail.com>
2021-09-20 17:58:46 +0200

    Add AdminGlossary empty component

---
+++ app_frontend/src/views/admin/AdminGlossary.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainTemplate from '../../templates/MainTemplate';
+
+const AdminGlossary = () => (
+  <MainTemplate>
+    <h3>Glossary</h3>
+  </MainTemplate>
+);
+
+export default AdminGlossary;
